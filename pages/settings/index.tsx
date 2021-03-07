84b5527f30be76c6329f2992733030a209acb129
Anna Vihrogonova <Anna.Vihrogonova@CLAP-MAC52.local>
2021-03-07 10:08:35 +0000

    add action buttons to settings page

--- pages/settings/index.tsx
+++ pages/settings/index.tsx
@@ -1,13 +1,16 @@
+import { Button } from "../../components/ui/atoms/button"
+import { Logo } from "../../components/ui/atoms/logo"
 import { FooterNavigation } from "../../components/ui/molecules/footer-navigation"
-import { ImageWrapper, SettingsImageWrapper } from "../../styles/styled-components"
+import { Centered, ImageWrapper, SettingsImageWrapper } from "../../styles/styled-components"
 
 
 const Settings = () => (
   <>
-    <ImageWrapper>
-      <SettingsImageWrapper src="/images/in-development.png"/>
-      <p>UNDER DEVELOPMENT</p>
-    </ImageWrapper>
+    <Logo />
+    <Centered>
+        <Button  type="primary" linkTo="/auth/login" name="Login"/>
+        <Button  type="primary" linkTo="/auth/signup" name="Register"/>
+      </Centered>
     <FooterNavigation />
   </>
 )
